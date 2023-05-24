d47e60a296f43810b4a66f448c5d31c39a4b9eaf
tan <bigmouth3033@gmail.com>
2023-05-24 13:17:23 +0700

    auctions eProject

---
+++ js/js.js
@@ -0,0 +1,69 @@
+let app = angular.module("auctionApp", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+	$routeProvider.when("/home", {
+		templateUrl: "./home.html",
+	});
+});
+
+app.service("DataService", function () {
+	let paintingData = null;
+
+	this.setPaintingData = function (data) {
+		paintingData = data;
+	};
+
+	this.getPaintingData = function () {
+		return paintingData;
+	};
+});
+
+app.run(function ($http, DataService) {
+	$http.get("../json/painting.json").then(
+		function (response) {
+			DataService.setPaintingData(response.data.abstractArt);
+		},
+		function (error) {
+			console.log("something went horribly wrong with json file", error);
+		}
+	);
+});
+
+function myTimer(date, obj) {
+	let now = new Date();
+	let future = new Date(date);
+
+	let timeLeft = future - now;
+
+	let timer = [];
+
+	let msLeft = timeLeft % 1000;
+
+	let sTotal = (timeLeft - msLeft) / 1000;
+	let sLeft = sTotal % 60;
+
+	let mTotal = (sTotal - sLeft) / 60;
+	let mLeft = mTotal % 60;
+
+	let hTotal = (mTotal - mLeft) / 60;
+	let hLeft = hTotal % 24;
+
+	let dTotal = (hTotal - hLeft) / 24;
+
+	timer.push(`${dTotal}d`);
+	timer.push(`${hLeft}h`);
+	timer.push(`${mLeft}m`);
+	timer.push(`${sLeft}s`);
+
+	obj.timer = timer.join(" : ");
+}
+
+app.controller("homeController", function ($scope, $interval, DataService) {
+	$scope.paintingData = DataService.getPaintingData();
+
+	for (let painting of $scope.paintingData) {
+		$interval(function () {
+			myTimer(painting.endDate, painting);
+		}, 1);
+	}
+});
